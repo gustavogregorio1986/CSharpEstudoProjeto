2e0d371f60c43c3043c4087fe0c7fb63dac928d0
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2025-12-31 19:29:43 -0300

    Add project files.

---
+++ pooMensagem/Entidade/Mensagem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooMensagem.Entidade
+{
+    public class Mensagem
+    {
+       public String TextoMensagem;
+
+       public void ExibirMensagem()
+       {
+            Console.WriteLine(this.TextoMensagem);
+        }
+    }
+}
---
+++ pooMensagem/Program.cs
@@ -0,0 +1,21 @@
+﻿using pooMensagem.Entidade;
+using System;
+
+namespace pooMensagem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Mensagem msg1, msg2;
+
+            msg1 = new Mensagem();
+            msg1.TextoMensagem = "Olá, Mundo!";
+            msg1.ExibirMensagem();
+
+            msg2 = new Mensagem();
+            msg2.TextoMensagem = "Programação Orientada a Objetos";
+            msg2.ExibirMensagem();
+        }
+    }
+}
