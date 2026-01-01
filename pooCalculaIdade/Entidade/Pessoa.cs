3f616d4fabcb3e48e7463fa5512727d3d5901f79
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-01-01 00:37:54 -0300

    Logica com desenvolvolvimento aprofundando no conhecimento

---
+++ pooCalculaIdade/Entidade/Pessoa.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooCalculaIdade.Entidade
+{
+    public class Pessoa
+    {
+		private int anoNasicmento;
+
+		public int AnoNascimento
+		{
+			get { return anoNasicmento; }
+			set { this.anoNasicmento = value; }
+		}
+
+		private String nome;
+
+		public String Nome
+		{
+			get { return nome; }
+			set { nome = value.ToUpper(); }
+		}
+
+		public void ExibirDados()
+		{
+            Console.WriteLine("Nome: " + this.Nome);
+            Console.WriteLine("Ano de Nascimento: " + this.anoNasicmento);
+            int idade = this.CalcularIdade();
+            Console.WriteLine("Idade: " + idade);
+		}
+
+		private int CalcularIdade()
+		{
+			DateTime data = DateTime.Now;
+			int ano = data.Year;
+			int idade = ano - this.anoNasicmento;
+			return idade;
+        }
+	}
+}
---
+++ pooCalculaIdade/Program.cs
@@ -0,0 +1,21 @@
+﻿using pooCalculaIdade.Entidade;
+using System;
+
+namespace pooCalculaIdade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa que calcula a Idade de uma pessoa");
+
+            Pessoa p = new Pessoa();
+            Console.WriteLine("Digite vo nome da pessoa: ");
+            p.Nome = Console.ReadLine();
+            Console.WriteLine("Digite o anod e nascimento: ");
+            p.AnoNascimento = int.Parse(Console.ReadLine());
+            p.ExibirDados();
+
+        }
+    }
+}
