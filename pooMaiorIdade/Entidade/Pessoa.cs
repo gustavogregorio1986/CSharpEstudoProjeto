cfb1a9d83a7f1d10e03c127760c6af82c4654837
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-01-09 12:45:55 -0300

    Logica de desenvolvimentod e sistemas e de software para aprimorar o conhecimento

---
+++ pooMaiorIdade/Entidade/Pessoa.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooMaiorIdade.Entidade
+{
+    public class Pessoa
+    {
+		private String nome;
+
+        public Pessoa()
+        {
+            this.nome = "";
+            this.Idade = 0;
+        }
+
+        public Pessoa(String nome, int idade)
+        {
+            this.nome = nome;
+            this.Idade = idade;
+        }
+
+        public String Nome
+		{
+			get { return nome; }
+			set { nome = value.ToUpper(); }
+		}
+
+		public int Idade { 
+            get { return Idade; }
+            set {  
+                if(value >= 0)
+                {
+                    Idade = value;
+                }
+                else
+                {
+                    this.Idade = 0;
+                }
+            } 
+        }
+		//Metodos
+		public void ExibirMaiorIdade()
+		{
+            Console.WriteLine("Nome: " + this.nome);
+        }
+    }
+}
---
+++ pooMaiorIdade/Program.cs
@@ -0,0 +1,60 @@
+﻿using pooMaiorIdade.Entidade;
+using System;
+
+namespace pooMaiorIdade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1;
+            Pessoa p2;
+            Pessoa p3;
+            Console.WriteLine("Determina qual é pessoa a mais velha");
+            //Leitura de dados da 1 pessoa
+            Console.WriteLine("Nome da 1º pessoa: ");
+            p1 = new Pessoa();
+            p1.Nome = Console.ReadLine();
+            Console.WriteLine("Idade da 1º pessoa: ");
+            p1.Idade = int.Parse(Console.ReadLine());
+            //Leitura de dados da 1 pessoa
+            Console.WriteLine("Nome da 2º pessoa: ");
+            p2 = new Pessoa();
+            p2.Nome = Console.ReadLine();
+            Console.WriteLine("Idade da 2º pessoa: ");
+            p2.Idade = int.Parse(Console.ReadLine());
+            //Leitura de dados da 3 pessoa
+            Console.WriteLine("Nome da 3º pessoa: ");
+            String nome = Console.ReadLine();
+            Console.WriteLine("Idade da 3º pessoa: ");
+            int idade = int.Parse(Console.ReadLine());
+            p3 = new Pessoa(nome, idade);
+            //Lófgica
+            if ((p1.Idade > p2.Idade) && (p1.Idade > p3.Idade))
+            {
+                Console.WriteLine("A pessoa mais velha é: ");
+                p1.ExibirMaiorIdade();
+            }
+            else
+            {
+                if ((p2.Idade > p3.Idade) && (p2.Idade > p1.Idade))
+                {
+                    Console.WriteLine("A pessoa mais belha é: ");
+                    p2.ExibirMaiorIdade();
+                }
+                else
+                {
+                    if((p3.Idade > p1.Idade) && (p3.Idade > p2.Idade))
+                    {
+                        Console.WriteLine("A pessoa m,ais velha é? ");
+                        p3.ExibirMaiorIdade();
+                    }
+                    else
+                    {
+                        Console.WriteLine("A pessoa m,ais velha é? ");
+                    }
+                }
+            }
+        }
+    }
+}
