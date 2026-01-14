382f12185f3920f6fdb6aa5ee8943bb330a76541
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-01-13 21:33:41 -0300

    Desenvolvimento de logica de programação na logica

---
+++ AreaRetangulo/Entidade/Retangulo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaRetangulo.Entidade
+{
+    public class Retangulo
+    {
+        public Retangulo()
+        {
+            
+        }
+
+        private int baseret;
+
+        public int Base
+        {
+            get { return baseret; }
+            set { 
+               if(value >= 0) baseret = value; 
+               else baseret = 0;
+            }
+        }
+
+        //altura do retângulo
+        private int altret;
+
+        public int Altura
+        {
+            get { return altret; }
+            set { 
+               if(value >= 0) altret = value;
+               else altret = 0;
+            }
+        }
+
+        public int Area
+        {
+            get { return altret*baseret; }
+        }
+
+        public void ExibirDados()
+        {
+            Console.WriteLine("Base: " + this.Base);
+            Console.WriteLine("Altura: " + this.Altura);
+            Console.WriteLine("Area: " + this.Area);
+        }
+    }
+}
