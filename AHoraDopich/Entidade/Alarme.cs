daa122beb2187e098229af0ec65936e9cd48609c
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-01-15 20:07:40 -0300

    Dersenvolvendo recurso da linguagem com Thread

---
+++ AHoraDopich/Entidade/Alarme.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AHoraDopich.Entidade
+{
+    public class Alarme
+    {
+        public Alarme()
+        {
+            this.Tempo = 3; //três minutos
+        }
+
+        public Alarme(int tempo)
+        {
+            this.Tempo = tempo;
+        }
+
+        //controla o tempo do Pitch
+        private int tempo;
+
+		public int Tempo
+		{
+			get { return tempo; }
+			set { tempo = 1000 * 60 * value; }
+		}
+
+        public void Iniciar()
+        {
+            Thread.Sleep(this.Tempo);
+            Console.Beep(150,1000);
+        }
+
+	}
+}
---
+++ AHoraDopich/Program.cs
@@ -0,0 +1,29 @@
+﻿using AHoraDopich.Entidade;
+using System;
+
+namespace AHoraDopich
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Alarme alarme = new Alarme();
+            String resposta = "S";
+            while(resposta!="N")
+            {
+                Console.Clear();
+                Console.WriteLine("A Hora do Pitch");
+                Console.WriteLine("Informe a duração do Pitch: ");
+                int tempo = int.Parse(Console.ReadLine());
+
+                alarme.Tempo = tempo;
+
+                alarme.Iniciar();
+                Console.WriteLine("Que pena!!! O seu tempo acabou!!");
+                Console.WriteLine("Executar o programa novamente: S/N");
+                resposta = Console.ReadLine().ToUpper().ToUpper();
+            }
+            
+        }
+    }
+}
