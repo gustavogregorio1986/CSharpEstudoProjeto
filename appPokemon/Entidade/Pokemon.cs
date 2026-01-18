780e03a6c02f77f2e2f36fa686e6aaa1c81ce0ff
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-01-18 17:54:09 -0300

    Desenvolvimentio para a logica de programação com a classe Pokemon e seu construtor com seus atributos

---
+++ appPokemon/Entidade/Pokemon.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooPokemonApp.Entidade
+{
+    public class Pokemon
+    {
+		public Pokemon()
+		{
+			this.Nome = "";
+            this.Descricao = "";
+        }
+
+        private string nome;
+
+		public String Nome
+		{
+			get { 
+			    	
+				return nome; 
+			}
+			set {
+                String texto = value.ToUpper();
+                nome = texto;
+            }
+		}
+
+		private String descricao; //armazena com valor da propriedade Descirção
+
+		public String Descricao //represneta a caracteristica Descrição do meu Pokemon
+        {
+			get { return descricao; }
+			set {
+				this.descricao = value.ToUpper(); 
+			}
+		}
+
+		public void ExibirDadosPokemon()
+		{
+			Console.WriteLine("Nome do Pkemon: " + this.Nome);
+			Console.WriteLine("Descrição: " + this.Descricao);
+        }
+
+    }
+}
---
+++ appPokemon/Program.cs
@@ -0,0 +1,17 @@
+﻿using pooPokemonApp.Entidade;
+using System;
+
+namespace pooPokemonApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           Pokemon p = new Pokemon();
+           p.Nome = "Bulbasauro";
+           p.Descricao = "Bulbasauro é um Pokémon do tipo grama/veneno da primeira geração...";
+
+           p.ExibirDadosPokemon();
+        }
+    }
+}
