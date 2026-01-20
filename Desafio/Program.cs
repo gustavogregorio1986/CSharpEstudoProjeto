c05b2b13cd53bcd22d2865a0025731b586e44dbb
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-01-20 20:31:20 -0300

    Desenvolvimento alheio de estudo

---
+++ Desafio/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Desafio
+{
+    internal class Program
+    {
+        public static string RemoverEspacos(string frase)
+        {
+            string[] word = frase.Replace("1", ");
+
+            return word;
+        }
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
