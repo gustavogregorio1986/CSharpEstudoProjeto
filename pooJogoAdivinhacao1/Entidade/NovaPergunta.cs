ef914ec13380766f6b0fbfbc38566137a612b264
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-01-20 21:37:53 -0300

    Desenvolvimento da logica de programação estudando

---
+++ pooJogoAdivinhacao1/Entidade/NovaPergunta.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooJogoAdivinhacao1.Entidade
+{
+    public class NovaPergunta:PerguntaJogo
+    {
+        public NovaPergunta()
+        {
+            this.Dica = "";
+
+        }
+
+        public NovaPergunta(String pergunta, String resposta, String dica):base(pergunta, resposta)
+        {
+            this.Dica = dica;
+            base.Pergunta = pergunta;
+        }
+
+        public String Dica { get; set; }
+    }
+}
---
+++ pooJogoAdivinhacao1/Program.cs
@@ -0,0 +1,22 @@
+﻿using pooJogoAdivinhacao1.Entidade;
+using System;
+using System.Collections.Generic;
+
+namespace pooJogoAdivinhacao1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<NovaPergunta> perguntas;
+            perguntas = CriarPerguntas();
+        }
+
+        static List<NovaPergunta> CriarPerguntas()
+        {
+            List<NovaPergunta> lista = new List<NovaPergunta>();
+            NovaPergunta p = new NovaPergunta("Qual a cor do céi?","Azul","Informe uma cor que comece com a letra A");
+            return lista;
+        }
+    }
+}
